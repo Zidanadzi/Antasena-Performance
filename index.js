75d105bc7fc99af9fd236aaf7ef22f72b8f8fde1
Ziddan <113444408+Zidanadzi@users.noreply.github.com>
2026-04-14 02:38:27 +0700

    feat: Remove unnecessary polyfills
    Removes Buffer and crypto polyfills from index.js as they are no longer required by the application's dependencies.

--- index.js
+++
@@ -1,7 +0,0 @@
-import 'react-native-get-random-values';
-import { Buffer } from 'buffer';
-global.Buffer = Buffer;
-import { registerRootComponent } from 'expo';
-import App from './src/App';
-
-registerRootComponent(App);
